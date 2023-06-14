144e7059e6b89657ee02645146be29945b3411b4
Duvan Villa <duvanvillaqd@gmail.com>
2023-06-13 19:38:58 -0500

    Actualización de repositorio

--- src/main/java/tarea/DctoVenta.java
+++ src/main/java/tarea/DctoVenta.java
@@ -5,18 +5,18 @@ import java.util.function.Function;
 
 public class DctoVenta {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Ingresa el valor de la venta : ");
+            double precioVenta = scanner.nextDouble();
 
-        System.out.println("Ingresa el valor de la venta : ");
-        double precioVenta = scanner.nextDouble();
+            System.out.println("De cuanto es el descuento a aplicar: ");
+            double valorDcto = scanner.nextDouble();
 
-        System.out.println("De cuanto es el descuento a aplicar: ");
-        double valorDcto = scanner.nextDouble();
+            Function<Double, Double> calcularPrecioFinal = precio -> precio * (1-(valorDcto/100));
+            double precioFinal =  calcularPrecioFinal.apply(precioVenta);
 
-        Function<Double, Double> calcularPrecioFinal = precio -> precio * (1-(valorDcto/100));
-        double precioFinal =  calcularPrecioFinal.apply(precioVenta);
-
-        System.out.println("El precio final con descuento es: " + precioFinal);
+            System.out.println("El precio final con descuento es: " + precioFinal);
+        }
 
     }
 }
--- src/main/java/tarea/NumMayorNumMenor.java
+++ src/main/java/tarea/NumMayorNumMenor.java
@@ -7,22 +7,23 @@ import java.util.Scanner;
 public class NumMayorNumMenor {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> numeros = new ArrayList<>();
+        try (Scanner scanner = new Scanner(System.in)) {
+            List<Integer> numeros = new ArrayList<>();
 
-        System.out.println("Ingresa la cantidad de números que deseas evaluar: ");
-        int cantNumeros = scanner.nextInt();
+            System.out.println("Ingresa la cantidad de números que deseas evaluar: ");
+            int cantNumeros = scanner.nextInt();
 
-        System.out.println("Ingresa los números uno por uno: ");
-        for (int i = 0; i < cantNumeros; i++){
-            int numero = scanner.nextInt();
-            numeros.add(numero);
+            System.out.println("Ingresa los números uno por uno: ");
+            for (int i = 0; i < cantNumeros; i++){
+                int numero = scanner.nextInt();
+                numeros.add(numero);
+            }
+
+            int numMayor = numeros.stream().max(Integer::compare).orElse(0);
+            int numMenor = numeros.stream().min(Integer::compare).orElse(0);
+
+            System.out.println("El número mayor es: " + numMayor);
+            System.out.println("El número menor es: " + numMenor);
         }
-
-        int numMayor = numeros.stream().max(Integer::compare).orElse(0);
-        int numMenor = numeros.stream().min(Integer::compare).orElse(0);
-
-        System.out.println("El número mayor es: " + numMayor);
-        System.out.println("El número menor es: " + numMenor);
     }
 }
